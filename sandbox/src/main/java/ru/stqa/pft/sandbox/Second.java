b823e28b5a3322dccab2e6391c5f1e8d75ecfcd0
kaverski <lena.kaverski@gmail.com>
2020-11-27 15:25:14 +0200

    Update back

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Second.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Second.java
@@ -1,8 +1,8 @@
 package ru.stqa.pft.sandbox;
 
-public class Second2 {
+public class Second {
     public static void main(String[] args) {
-        System.out.println("Hello2 Java training");
+        System.out.println("Hello Java training");
         System.out.println();
     }
 }
