4d4206053425fbb75f154dbeabac16961740a2ff
kaverski <lena.kaverski@gmail.com>
2020-11-27 15:17:05 +0200

    Update ignore

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Second.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Second.java
@@ -3,5 +3,6 @@ package ru.stqa.pft.sandbox;
 public class Second {
     public static void main(String[] args) {
         System.out.println("Hello Java training");
+        System.out.println();
     }
 }
