d09929e345e2ebde2dc095bd2df60f2d08976a51
kaverski <lena.kaverski@gmail.com>
2020-11-29 12:17:05 +0200

    Adding build.gradle

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointDemo.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDemo.java
@@ -1,13 +1,9 @@
 package ru.stqa.pft.sandbox;
 
-public class PointTest {
+public class PointDemo {
     public static void main(String[] args) {
         Point point1 = new Point(5,3);
         Point point2 = new Point(10,12);
-        //function
-        System.out.println(Point.distance(point1, point2));
-
-        //method
         System.out.println(point1.distance(point2));
     }
 }
