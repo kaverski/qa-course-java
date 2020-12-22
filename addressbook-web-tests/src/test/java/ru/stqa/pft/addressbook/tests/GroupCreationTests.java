f866547bcfa0b4dc4cec25aab5d51480a67a4d48
kaverski <lena.kaverski@gmail.com>
2020-12-22 14:57:06 +0200

    Added pre-conditions and checks

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -8,9 +8,6 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().goToGroupPage();
-        app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("testDefault", null, null));
-        app.getGroupHelper().submitGroupForm();
-        app.getGroupHelper().returnToGroupPage();
+        app.getGroupHelper().createGroup(new GroupData("test555", null, null));
     }
 }
